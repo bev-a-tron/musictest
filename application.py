3052cb98dabd88469632e7c9a4f64ee0d2e4c158
bev-a-tron <beverly.a.lau@gmail.com>
2012-06-23 12:37:43 -0400

    shuffle order moved to index

--- application.py
+++ application.py
@@ -9,13 +9,15 @@ app.filenames=['jsuc5P',\
                'kQYPf7',\
                'lSFaDi',\
                'mTdR8a']
-shuffle(app.filenames)
 app.num=0
 
 @app.route('/')
 @app.route('/index',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
+        app.order=range(0,5)
+        shuffle(app.order)
+        print app.order
         return render_template('userinfo.html')
     else:
         app.progvars['name'] = request.form['name']
@@ -24,6 +26,7 @@ def index():
 
 @app.route('/main')
 def main():
+    print app.order
     if app.num == len(app.filenames): return render_template('end.html')
     app.num += 1
     return redirect(url_for('item'))
