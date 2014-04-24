756ce10d7e5c3357abf2c9bb95453464cd435573
Asif R Choudhury <choudhury.asif@gmail.com>
2014-04-24 09:56:21 -0500

    rand connects to postgresql locally

--- rand.py
+++ rand.py
@@ -1,11 +1,20 @@
 #Bismillah
 import os
+import psycopg2
 import urlparse
 
-dburl = os.environ.get("DATABASE_URL", "postgresql://postgresql@localhost/test")
+dburl = os.environ.get("DATABASE_URL", "postgresql://postgres@localhost/musictest")
 
-dbstring = urlparse.urlparse(dburl)
+url = urlparse.urlparse(dburl)
 
-print dbstring
+print url
 
-print "username is " + dbstring.username
+print "username is " + url.username
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
